bc2879fdae4d4eb503654d5894dbd2eb3d242d93
yanhao <97150477@qq.com>
2019-03-01 21:00:43 +0800

    第一次提交

---
+++ src/main/java/com/itheima/mgtest/MGTest.java
@@ -0,0 +1,12 @@
+package com.itheima.mgtest;
+
+import java.io.Serializable;
+
+public class MGTest implements Serializable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
